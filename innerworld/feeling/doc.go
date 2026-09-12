da121a998858f833487d3244c22e44f5ae7a807d
Arianna Method <theariannamethod@gmail.com>
2026-09-12 22:56:15 +0300

    Delete innerworld directory

--- innerworld/feeling/doc.go
+++
@@ -1,5 +0,0 @@
-// Package feeling contains the optional Julia-backed feeling math for the High brain.
-//
-// Build with -tags julia to compile the embedded libjulia backend. The default build
-// keeps the runtime on the Go lexical proxy and does not require Julia to be installed.
-package feeling
